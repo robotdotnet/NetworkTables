767e3960d9c80d12ee8a41108471775f91fa8c44
Thad House <thadhouse1@gmail.com>
2016-05-04 20:11:20 -0700

    Updates Managed description

--- NetworkTables/Properties/AssemblyInfo.cs
+++ NetworkTables/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("NetworkTables")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A Managed version of the NetworkTables3 Protocol")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("RobotDotNet")]
 [assembly: AssemblyProduct("NetworkTables")]
