d1c1b01085b9d83cd1eb87d852bf9fd032de5be8
Thad House <thadhouse1@gmail.com>
2016-05-04 20:02:02 -0700

    Fixes SandCastle versioning and Author for NuGet

--- NetworkTables/Properties/AssemblyInfo.cs
+++ NetworkTables/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("NetworkTables")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("RobotDotNet")]
 [assembly: AssemblyProduct("NetworkTables")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
