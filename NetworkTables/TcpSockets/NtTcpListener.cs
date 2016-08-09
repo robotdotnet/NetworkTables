79dd3e6b52c08bd4bd1ffa801ef665739292c070
Thad House <thadhouse1@gmail.com>
2016-08-08 22:39:31 -0700

    Disables ExclusiveAddressUse

--- NetworkTables/TcpSockets/NtTcpListener.cs
+++ NetworkTables/TcpSockets/NtTcpListener.cs
@@ -41,7 +41,7 @@ namespace NetworkTables.TcpSockets
             {
                 return;
             }
-
+            m_serverSocket.ExclusiveAddressUse = false;
             m_serverSocket.Bind(m_serverSocketEp);
             try
             {
