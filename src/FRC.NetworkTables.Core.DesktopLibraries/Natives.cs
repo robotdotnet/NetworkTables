ffd2b49766ea1998d140d587b38b12b4815ce508
Thad House <ThadHouse@users.noreply.github.com>
2018-06-01 17:56:13 -0700

    Adds Desktop Project, and moves core project to its old location

---
+++ src/FRC.NetworkTables.Core.DesktopLibraries/Natives.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("FRC.NetworkTables.Core")]
+
+namespace FRC.NetworkTables.DesktopLibraries
+{
+  internal class Natives
+  {
+
+  }
+}
